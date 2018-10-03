864f3d46e69482d565af87fe67e18bba038139d0
Adam Duff <j.adam.duff@gmail.com>
2018-10-03 17:41:23 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:create]
+
+  post '/logout', :to => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
