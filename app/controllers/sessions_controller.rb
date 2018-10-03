0c40d6446c0e49d71999dbf20fd5affb836b6517
Adam Duff <j.adam.duff@gmail.com>
2018-10-03 17:57:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
 
-  before_action :require_login, only: [:create]
-
   def new
   end
 
   def create
     session[:name] = params[:name]
-    redirect_to root_path
+    if session[:name].nil? || session[:name].empty?
+      redirect_to login_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
